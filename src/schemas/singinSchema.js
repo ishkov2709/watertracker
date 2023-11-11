ba676be473f3b6f02d6e5d82a27e4178d1131faf
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-11-11 16:07:06 +0200

    fixed

--- src/schemas/singinSchema.js
+++ src/schemas/singinSchema.js
@@ -5,7 +5,7 @@ export const singinSchema = Yup.object().shape({
     .matches(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Invalid email')
     .required('Email is required'),
   password: Yup.string()
-    .matches(/^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$/)
+    .matches(/^(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,64}$/)
     .min(8, 'Too short password')
     .max(64, 'Too long password')
     .required('Password is required'),
