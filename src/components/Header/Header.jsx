6854b5cd6c149f272385743650dab826a6ecf062
TamaraKhrustalyova <tamara.khrustalyova@gmail.com>
2023-11-11 18:22:47 +0200

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,28 @@
 import Container from 'components/Container';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import UserAuth from 'components/UserAuth/UserAuth';
 
 const Header = () => {
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
   return (
     <header>
-      <Container>Header</Container>
+      <Container>
+            {isLoggedIn ? (
+          <nav>
+            <Link to='/Home'>
+                <button><img src='./img/logo/Logo.png' alt='logo'/></button>
+            </Link>
+          </nav>
+        ) : (
+            <nav>
+              <Link to='/'>
+                  <button><img src='./img/logo/Logo.png' alt='logo'/></button>
+              </Link>
+          </nav>
+        )}
+        <UserAuth/>
+      </Container>
     </header>
   );
 };
---
+++ src/components/UserAuth/UserAuth.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Circle = styled.div`
+  display: flex;
+  width: 100px;
+  height: 100px;
+  background-color: white;
+  border-radius: 50%;
+`;
