b2291b3b2703ca41a2c013a00883513fcdf9875d
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-11-11 23:08:03 +0200

    create DaysGeneralStats component

---
+++ src/components/DaysGeneralStats/DaysGeneralStats.styled.jsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+import { color } from 'styles/colors';
+
+export const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  width: 292px;
+  height: 188px;
+
+  padding: 24px 13px;
+
+  background-color: ${color.primary.white};
+
+  border-radius: 10px;
+
+  box-shadow: 0 4px 4px 0 #407bff4d;
+
+  @media screen and (min-width: 768px) {
+    padding: 24px 16px;
+  }
+`;
+
+export const DateMonthTitle = styled.p`
+  color: ${color.primary.brue};
+`;
+
+export const MarkTitle = styled.p`
+  color: ${color.primary.black};
+`;
+
+export const Mark = styled.span`
+  color: ${color.primary.brue};
+
+  font-weight: 500;
+  font-size: 18px;
+`;
+
+export const Btn = styled.button`
+  position: absolute;
+  background: transparent;
+  border: none;
+
+  top: 21px;
+  right: 13px;
+
+  @media screen and (min-width: 768px) {
+    right: 16px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    display: none;
+  }
+
+  & .icon {
+    transform: rotate(45deg);
+  }
+`;
