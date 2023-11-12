de184ebf25ccfb45c21a6df21cf207a62fbe1ebd
FedorovMykhailo <fedorov.mykhailo@gmail.com>
2023-11-12 18:52:09 +0200

    visual component WaterRatioPanel
    Візуал компонента WaterRatioPanel.
    Функціонал в опрацюванні.

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import MonthStatsTable from 'components/MonthStatsTable';
+import WaterRatioPanel from 'components/WaterRatioPanel/WaterRatioPanel';
 
 const HomePage = () => {
   return (
     <div>
       <MonthStatsTable />
+      <WaterRatioPanel />
     </div>
   );
 };
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,7 +2,8 @@ import { useSelector } from 'react-redux';
 import SigninPage from './SigninPage';
 
 const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
-  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  // const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  const isLoggedIn = true;
   const isRefreshing = useSelector(state => state.auth.isRefreshing);
   console.log(isLoggedIn);
   console.log(isRefreshing);
