ae18885de704815efa186a5c9037d5ac836590c2
MHatalyak <matronahu@gmail.com>
2023-11-12 17:20:18 +0200

    added styles to sign up and sign in pages

--- src/components/SigninPage/Auth.styled.js
+++ src/components/SigninPage/Auth.styled.js
@@ -1,37 +1,141 @@
 import styled from 'styled-components';
+import { color } from 'styles/colors';
+import imgHomeBgButle from '../../img/background/homePage/desktop/bgHomeButleDesc.png';
+import imgHomeBgButleTablet from '../../img/background/homePage/tab/bgHomeTabButle.png';
+import imgHomeBgButlePhone from '../../img/background/homePage/mob/bgHomePhoneButle.png';
+import { Link } from 'react-router-dom';
+import { Form } from 'formik';
 
-export const Title = styled.h1`
-  margin-bottom: 20px;
+export const Box = styled.div`
+  min-height: 100vh;
+
+  background-image: url(${imgHomeBgButlePhone});
+  background-size: 100%;
+  background-position: center bottom 20px;
+  background-repeat: no-repeat;
+
+  @media screen and (min-width: 768px) {
+    background-image: url(${imgHomeBgButleTablet});
+    background-size: 100%;
+    background-position: right -80px bottom 16px;
+    background-repeat: no-repeat;
+  }
+
+  @media screen and (min-width: 1440px) {
+    background-image: url(${imgHomeBgButle});
+    background-size: 80%;
+    background-position: bottom 80% left -51%;
+    background-repeat: no-repeat;
+  }
 `;
 
-export const Form = styled.form`
-  margin-bottom: 40px;
-  padding: 10px;
-  width: 500px;
-  margin-left: auto;
-  margin-right: auto;
+export const Title = styled.h1`
+  margin-bottom: 16px;
+  display: block;
+  font-size: 26px;
+  font-weight: 500;
+  line-height: 32px;
+
+  @media screen and (min-width: 1440px) {
+    min-width: 336px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    min-width: 384px;
+    margin-right: 198px;
+    margin-left: 833px;
+  }
+`;
+
+export const StyledForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+  align-items: center;
+  @media screen and (min-width: 768px) {
+    align-items: flex-start;
+  }
+
+  @media screen and (min-width: 1440px) {
+    align-items: flex-end;
+  }
 `;
 
 export const Label = styled.label`
   display: block;
-  margin-bottom: 5px;
-  margin-top: 10px;
+  margin-bottom: 8px;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 24px;
 `;
 
-export const Input = styled.input`
+export const StyledInput = styled.input`
   padding: 8px;
-  font-size: 16px;
-  margin-left: 10px;
-
+  padding: 12px 10px;
+  border: 1px solid ${color.secondary.gray};
+  background-color: ${color.primary.white};
+  border-radius: 6px;
   margin-bottom: 10px;
+  font-weight: 400;
+  min-width: 280px;
+  font-size: 16px;
+  line-height: 20px;
+
+  &::placeholder {
+    color: ${color.secondary.azure};
+  }
+
+  &:last-child {
+    margin-bottom: 16px;
+  }
+
+  @media screen and (min-width: 768px) {
+    min-width: 336px;
+    margin-bottom: 16px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    min-width: 384px;
+  }
 `;
 
-export const RegisterButton = styled.button`
-  margin-top: 20px;
-  margin-right: 10px;
-  padding: 10px 20px;
-  font-size: 16px;
+export const SigninButton = styled.button`
+  background-color: ${color.primary.brue};
+  box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
+  border-radius: 10px;
   border: none;
-  border-radius: 4px;
-  cursor: pointer;
+  min-width: 280px;
+  padding: 8px 30px;
+  color: ${color.primary.white};
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+  @media screen and (min-width: 768px) {
+    padding: 10px 30px;
+    min-width: 336px;
+    font-size: 18px;
+    line-height: 24px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    min-width: 384px;
+  }
+`;
+
+export const LinkToPage = styled(Link)`
+  display: block;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 20px;
+  text-decoration: none;
+  margin-bottom: 6px;
+  color: ${color.primary.brue};
+  @media screen and (min-width: 1440px) {
+    margin-right: 198px;
+    margin-left: 833px;
+  }
+`;
+export const ErrorM = styled.div`
+  color: ${color.secondary.tomato};
+  margin-bottom: 8px;
 `;
--- src/components/SigninPage/SignIn.jsx
+++ src/components/SigninPage/SignIn.jsx
@@ -4,56 +4,117 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { errorSelector } from '../../store/auth/selectors';
 
-import { RegisterButton, Label, Input, Form, Title } from './Auth.styled';
+import {
+  Box,
+  Label,
+  StyledForm,
+  StyledInput,
+  SigninButton,
+  Title,
+  LinkToPage,
+  ErrorM,
+} from './Auth.styled';
+import { Wrapper } from 'components/HomePage/HomePage.styled';
+import { Formik, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
-function Signin({ signin }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+// function Signin({ signin }) {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const error = useSelector(errorSelector);
+
+//   const handleSubmit = e => {
+//     e.preventDefault();
+//     signin({ email, password });
+//   };
+const validationSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Email is required'),
+  password: Yup.string().required('Password is required'),
+});
+
+const initialValues = {
+  email: '',
+  password: '',
+};
+
+const Signin = ({ signin }) => {
   const error = useSelector(errorSelector);
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    signin({ email, password });
+  const handleSubmit = (values, { setSubmitting }) => {
+    signin(values);
+    setSubmitting(false);
   };
   return (
-    <Container>
-      <div>
-        {error && <h4>{error}</h4>}
-        <Title>Login</Title>
-        <Form onSubmit={handleSubmit}>
+    <Wrapper>
+      <Container>
+        <Box>
           <div>
-            <Label>Enter your email</Label>
-            <Input
-              type="email"
-              name="email"
-              value={email}
-              placeholder="Email"
-              onChange={({ target: { value } }) => setEmail(value)}
-              required
-            />
+            {error && <h4>{error}</h4>}
+            <Title>Sign In</Title>
+            {/* <Form onSubmit={handleSubmit}>
+              <div>
+                <Label>Enter your email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  value={email}
+                  placeholder="Email"
+                  onChange={({ target: { value } }) => setEmail(value)}
+                  required
+                />
+              </div>
+              <div>
+                <Label>Enter your password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  value={password}
+                  placeholder="Password"
+                  onChange={({ target: { value } }) => setPassword(value)}
+                  required
+                />
+              </div>
+              <RegisterButton type="submit">Sign In</RegisterButton>
+            </Form> */}
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}
+            >
+              <StyledForm>
+                <div>
+                  <Label>Enter your email</Label>
+                  <Field
+                    as={StyledInput}
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                  <ErrorMessage name="email" component={ErrorM} />
+                </div>
+                <div>
+                  <Label>Enter your password</Label>
+                  <Field
+                    as={StyledInput}
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    required
+                  />
+                  <ErrorMessage name="password" component={ErrorM} />
+                </div>
+                <SigninButton type="submit">Sign In</SigninButton>
+              </StyledForm>
+            </Formik>
+
+            <LinkToPage to="/forgot-password">Forgot password?</LinkToPage>
+
+            <LinkToPage to="/signup">Sign up</LinkToPage>
           </div>
-          <div>
-            <Label>Enter your password</Label>
-            <Input
-              type="password"
-              name="password"
-              value={password}
-              placeholder="Password"
-              onChange={({ target: { value } }) => setPassword(value)}
-              required
-            />
-          </div>
-          <RegisterButton type="submit">Sign In</RegisterButton>
-        </Form>
-        <p>
-          <Link to="/forgot-password">Forgot password?</Link>
-        </p>
-        <p>
-          <Link to="/signup">Sign up</Link>
-        </p>
-      </div>
-    </Container>
+        </Box>
+      </Container>
+    </Wrapper>
   );
-}
+};
 
 export default Signin;
