2e851d3f29d08f5f446df6280e94712762815a25
StasLiubinetskyi <stasliubinetskyi@gmail.com>
2023-11-13 20:07:50 +0200

    test

---
+++ src/components/DailyNormaModal/DailyNormaModal.jsx
@@ -0,0 +1,192 @@
+import { Box } from 'components/common/Container/Container.styled';
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+  MyDailyNorma,
+  XMarkoutlineIcon1,
+  MyDailyNormaParent,
+  FrameGroup,
+  FrameDiv,
+  FrameParent1,
+  ForGirlParent,
+  ForGirl,
+  Vm003T04,
+  ForMan,
+  VIsTheVolumeOfTheWaterNWrapper,
+  VIsTheContainer,
+  Span,
+  VIsThe1,
+  CalculateYourRateParent,
+  CalculateYourRate1,
+  FrameParent2,
+  GroupParent,
+  FrameChild,
+  ForGirl1,
+  EllipseParent,
+  FrameItem,
+  ForMan1,
+  YourWeightInKilogramsParent,
+  YourWeightIn1,
+  Wrapper,
+  TheTimeOf1,
+  TheRequiredAmount1,
+  L,
+  SaveWrapper,
+  Save,
+  WriteDownHow1,
+  Frame1,
+} from './DailyNormaModal.styled';
+
+const DailyNormaModal = ({ setModalOpen }) => {
+  const [weight, setWeight] = useState(0);
+  const [activityTime, setActivityTime] = useState(0);
+  const [calculatedWaterAmount, setCalculatedWaterAmount] = useState(0);
+  const [drankWaterAmount, setDrankWaterAmount] = useState(0);
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const handleSave = () => {
+    console.log('Data to be sent:', {
+      weight,
+      activityTime,
+      calculatedWaterAmount,
+      drankWaterAmount,
+    });
+
+    const backendSuccess = true;
+
+    if (backendSuccess) {
+      closeModal();
+    } else {
+      console.error('Failed to save data. Please try again.');
+    }
+  };
+
+  const handleWeightChange = event => {
+    setWeight(event.target.value);
+  };
+
+  const handleActivityTimeChange = event => {
+    setActivityTime(event.target.value);
+  };
+
+  const handleDrankWaterChange = event => {
+    setDrankWaterAmount(event.target.value);
+  };
+
+  const calculateWaterAmount = useCallback(() => {
+    const calculatedAmount = weight * 0.03 + activityTime * 0.4;
+    setCalculatedWaterAmount(calculatedAmount.toFixed(2));
+  }, [weight, activityTime]);
+
+  useEffect(() => {
+    calculateWaterAmount();
+  }, [calculateWaterAmount]);
+
+  useEffect(() => {
+    setModalOpen(true);
+  });
+
+  return (
+    <Box>
+      <MyDailyNormaParent>
+        <MyDailyNorma>My daily norma</MyDailyNorma>
+        <XMarkoutlineIcon1
+          alt=""
+          src="x-mark/outline.svg"
+          onClick={() => setModalOpen(false)}
+        />
+      </MyDailyNormaParent>
+      <FrameGroup>
+        <FrameDiv>
+          <FrameParent1>
+            <ForGirlParent>
+              <ForGirl>For girl:</ForGirl>
+              <Vm003T04>V=(M*0,03) + (T*0,4)</Vm003T04>
+            </ForGirlParent>
+            <ForGirlParent>
+              <ForMan>For man:</ForMan>
+              <Vm003T04>V=(M*0,04) + (T*0,6)</Vm003T04>
+            </ForGirlParent>
+          </FrameParent1>
+          <VIsTheVolumeOfTheWaterNWrapper>
+            <VIsTheContainer>
+              <Span>*</Span>
+              <VIsThe1>
+                {' '}
+                V is the volume of the water norm in liters per day, M is your
+                body weight, T is the time of active sports, or another type of
+                activity commensurate in terms of loads (in the absence of
+                these, you must set 0)
+              </VIsThe1>
+            </VIsTheContainer>
+          </VIsTheVolumeOfTheWaterNWrapper>
+        </FrameDiv>
+        <CalculateYourRateParent>
+          <CalculateYourRate1>Calculate your rate:</CalculateYourRate1>
+          <FrameParent2>
+            <FrameParent1>
+              <GroupParent>
+                <FrameChild alt="" src="Group 11.svg" />
+                <ForGirl1>For girl</ForGirl1>
+              </GroupParent>
+              <EllipseParent>
+                <FrameItem type="radio" />
+                <ForMan1>For man</ForMan1>
+              </EllipseParent>
+            </FrameParent1>
+            <YourWeightInKilogramsParent>
+              <YourWeightIn1>Your weight in kilograms:</YourWeightIn1>
+              <Wrapper>
+                <input
+                  type="text"
+                  value={weight}
+                  onChange={handleWeightChange}
+                />
+              </Wrapper>
+            </YourWeightInKilogramsParent>
+            <YourWeightInKilogramsParent>
+              <TheTimeOf1>
+                The time of active participation in sports or other activities
+                with a high physical. load:
+              </TheTimeOf1>
+              <Wrapper>
+                <input
+                  type="text"
+                  value={activityTime}
+                  onChange={handleActivityTimeChange}
+                />
+              </Wrapper>
+            </YourWeightInKilogramsParent>
+            <EllipseParent>
+              <TheRequiredAmount1>
+                The required amount of water in liters per day:
+              </TheRequiredAmount1>
+              <L>{calculatedWaterAmount} L</L>
+            </EllipseParent>
+          </FrameParent2>
+        </CalculateYourRateParent>
+        <CalculateYourRateParent>
+          <WriteDownHow1>
+            Write down how much water you will drink:
+          </WriteDownHow1>
+          <Frame1>
+            <Wrapper>
+              <input
+                type="text"
+                value={drankWaterAmount}
+                onChange={handleDrankWaterChange}
+              />
+            </Wrapper>
+          </Frame1>
+        </CalculateYourRateParent>
+      </FrameGroup>
+      <SaveWrapper>
+        <Save onClick={handleSave}>Save</Save>
+      </SaveWrapper>
+    </Box>
+  );
+};
+
+export default DailyNormaModal;
---
+++ src/components/DailyNormaModal/DailyNormaModal.styled.jsx
@@ -0,0 +1,291 @@
+import styled from 'styled-components';
+
+export const ModalContent = styled.div`
+  background-color: #fff;
+  border: 1px solid #ddd;
+  padding: 20px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1000;
+  width: 300px;
+
+  @media screen and (min-width: 768px) {
+    width: 500px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 600px;
+  }
+`;
+
+export const CloseButton = styled.button`
+  background: none;
+  border: none;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+  font-size: 20px;
+  color: #333;
+`;
+
+export const ModalTitle = styled.h2`
+  color: #333;
+`;
+
+export const MyDailyNorma = styled.div`
+  line-height: 32px;
+  font-weight: 500;
+  width: 180px;
+  flex-shrink: 0;
+`;
+
+export const XMarkoutlineIcon1 = styled.img`
+  width: 24px;
+  height: 24px;
+  overflow: hidden;
+  flex-shrink: 0;
+`;
+
+export const MyDailyNormaParent = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 20px;
+  font-size: 22px;
+`;
+
+export const ForGirl = styled.div`
+  line-height: 20px;
+  width: 54px;
+  flex-shrink: 0;
+`;
+
+export const Vm003T04 = styled.div`
+  font-size: 18px;
+  line-height: 24px;
+  color: #407bff;
+  width: 168px;
+  flex-shrink: 0;
+`;
+
+export const ForGirlParent = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 4px;
+`;
+
+export const ForMan = styled.div`
+  line-height: 20px;
+  width: 64px;
+  flex-shrink: 0;
+`;
+
+export const FrameParent1 = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 24px;
+`;
+
+export const Span = styled.span``;
+
+export const VIsThe1 = styled.span`
+  color: #8f8f8f;
+`;
+
+export const VIsTheContainer = styled.div`
+  line-height: 16px;
+  display: inline-block;
+  width: 100%;
+`;
+
+export const VIsTheVolumeOfTheWaterNWrapper = styled.div`
+  border-radius: 10px;
+  border: 1px solid #d7e3ff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  font-size: 12px;
+  color: #407bff;
+`;
+
+export const FrameDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 12px;
+  font-size: 16px;
+`;
+
+export const CalculateYourRate1 = styled.div`
+  line-height: 20px;
+  font-weight: 500;
+  display: inline-block;
+  width: 156px;
+`;
+
+export const FrameChild = styled.img`
+  width: 14px;
+  height: 14px;
+`;
+
+export const ForGirl1 = styled.div`
+  line-height: 20px;
+`;
+
+export const GroupParent = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 8px;
+`;
+
+export const FrameItem = styled.div`
+  border-radius: 50%;
+  background-color: #fff;
+  border: 1px solid #407bff;
+  box-sizing: border-box;
+  width: 14px;
+  height: 14px;
+`;
+
+export const ForMan1 = styled.div`
+  line-height: 20px;
+  display: inline-block;
+  width: 60px;
+  flex-shrink: 0;
+`;
+
+export const EllipseParent = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 6px;
+`;
+
+export const YourWeightIn1 = styled.div`
+  line-height: 20px;
+  display: inline-block;
+  width: 182px;
+`;
+
+export const Div = styled.div`
+  line-height: 20px;
+  display: inline-block;
+  width: 524px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: 12px 10px;
+  color: #407bff;
+`;
+
+export const YourWeightInKilogramsParent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 8px;
+`;
+
+export const TheTimeOf1 = styled.div`
+  line-height: 20px;
+  display: inline-block;
+  width: 544px;
+`;
+
+export const TheRequiredAmount1 = styled.div`
+  line-height: 20px;
+  display: inline-block;
+  width: 328px;
+  flex-shrink: 0;
+`;
+
+export const L = styled.b`
+  font-size: 18px;
+  line-height: 24px;
+  display: inline-block;
+  color: #407bff;
+  width: 42px;
+  flex-shrink: 0;
+`;
+
+export const FrameParent2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 16px;
+  font-size: 16px;
+`;
+
+export const CalculateYourRateParent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 16px;
+`;
+
+export const WriteDownHow1 = styled.div`
+  line-height: 20px;
+  font-weight: 500;
+  display: inline-block;
+  width: 348px;
+`;
+
+export const Frame1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: 12px 0px;
+  font-size: 16px;
+  color: #407bff;
+`;
+
+export const FrameGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 24px;
+`;
+
+export const Save = styled.div`
+  line-height: 24px;
+  font-weight: 500;
+  display: inline-block;
+  width: 100px;
+  flex-shrink: 0;
+`;
+
+export const SaveWrapper = styled.div`
+  border-radius: 10px;
+  background-color: #407bff;
+  box-shadow: 0px 4px 8px rgba(64, 123, 255, 0.34);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 10px 20px;
+  text-align: center;
+  color: #fff;
+  cursor: pointer;
+`;
