9eee68ce041aaa6415d43923a6aa4c13b1bd460b
Supreme-cake-being <XGon20987@gmail.com>
2023-11-13 10:49:07 +0200

    added button types

--- src/components/common/Button/Button.styled.jsx
+++ src/components/common/Button/Button.styled.jsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
-const Btn = styled.button(({ width }) => ({
+const Btn = styled.button(({ width, type }) => ({
   width: `${width}px`,
+
+  color: type === 'cancel' ? '#407BFF' : '#fff',
+  backgroundColor:
+    type === 'cancel' ? '#D7E3FF' : type === 'delete' ? '#EF5050' : '#407bff',
 }));
 
 export const ButtonContainer = styled(Btn)`
@@ -12,14 +16,16 @@ export const ButtonContainer = styled(Btn)`
   justify-content: center;
   gap: 10px;
 
-  color: #fff;
   border-radius: 10px;
-  background: #407bff;
   box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
 
   font-weight: 500;
   font-size: 18px;
   line-height: 1.33;
+
+  &:hover {
+    box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.54);
+  }
 
   @media screen and (max-width: 767px) {
     width: 100%;
