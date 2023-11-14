4132be3ae344d3548f9b60afbe6d4f71c71d2fef
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-11-14 13:17:09 +0200

    fixed baseUrl on requests

--- src/store/auth/thunk.js
+++ src/store/auth/thunk.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-//axios.defaults.baseURL = `https://connections-api.herokuapp.com`;
+//axios.defaults.baseURL = 'https://water-tracker.onrender.com/api';
 
 export const setToken = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
