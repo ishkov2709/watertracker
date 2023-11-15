ed5ff89cf600e121d475d781e370957d1f7f6bdc
Karr4 but from another universe <XGon20987@gmail.com>
2023-11-15 20:55:10 +0200

    small url change

--- src/store/auth/thunk.js
+++ src/store/auth/thunk.js
@@ -1,27 +1,26 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-axios.defaults.baseURL = `https://water-tracker.onrender.com/`;
+axios.defaults.baseURL = `https://water-tracker.onrender.com/api`;
 
 export const setToken = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-
 };
 
 export const deleteToken = () =>
   (axios.defaults.headers.common.Authorization = '');
 
 export const signInUser = createAsyncThunk('auth/signin', async user => {
-  const response = await axios.post('/api/users/login', user);
+  const response = await axios.post('/users/login', user);
   return response.data;
 });
 
 export const signUpUser = createAsyncThunk('auth/signup', async newUser => {
-  const response = await axios.post('/api/users/signup', newUser);
+  const response = await axios.post('/users/signup', newUser);
   return response.data;
 });
 
 export const logOut = createAsyncThunk(`auth/logout`, async token => {
-  return await axios.post(`/api/users/logout`, token);
+  return await axios.post(`/users/logout`, token);
 });
 
 export const refreshUser = createAsyncThunk(
@@ -34,7 +33,7 @@ export const refreshUser = createAsyncThunk(
     }
     try {
       setToken(persistedToken);
-      const res = await axios.get('/api/users/current');
+      const res = await axios.get('/users/current');
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
