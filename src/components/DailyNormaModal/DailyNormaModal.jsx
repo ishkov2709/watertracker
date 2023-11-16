5614e3d94254e3964bf4230be1027c77916f1149
StasLiubinetskyi <stasliubinetskyi@gmail.com>
2023-11-16 09:57:55 +0200

    up

--- src/components/DailyNormaModal/DailyNormaModal.jsx
+++ src/components/DailyNormaModal/DailyNormaModal.jsx
@@ -3,15 +3,39 @@ import { Box } from 'components/common/Container/Container.styled';
 import FormInput from 'components/common/FormInput/FormInput';
 import Button from 'components/common/Button';
 import Icon from 'components/common/Icon/Icon';
-import * as Styled from './DailyNormaModal.styled';
-const DailyNormaModal = ({ setModalOpen }) => {
+import Modal from 'components/common/Modal';
+
+import {
+  ModalHeader,
+  CloseButton,
+  DailyNormas,
+  Forman,
+  ColorTextNormal,
+  VolumeNorm,
+  TextNormal,
+  CalculateYourRate,
+  FrameParent,
+  FrameItem,
+  YourWeight,
+  YourTime,
+  Required,
+  L,
+  Write,
+  Frame,
+  SaveWrapper,
+} from './DailyNormaModal.styled';
+
+const DailyNormaModal = ({ setModalOpen: propSetModalOpen }) => {
+  const [modalOpen, setModalOpen] = useState(false);
   const [weight, setWeight] = useState(0);
   const [activityTime, setActivityTime] = useState(0);
   const [calculatedWaterAmount, setCalculatedWaterAmount] = useState(0);
   const [drankWaterAmount, setDrankWaterAmount] = useState(0);
+
   const closeModal = () => {
     setModalOpen(false);
   };
+
   const handleSave = () => {
     console.log('Data to be sent:', {
       weight,
@@ -19,6 +43,7 @@ const DailyNormaModal = ({ setModalOpen }) => {
       calculatedWaterAmount,
       drankWaterAmount,
     });
+
     const backendSuccess = true;
     if (backendSuccess) {
       closeModal();
@@ -26,100 +51,110 @@ const DailyNormaModal = ({ setModalOpen }) => {
       console.error('Failed to save data. Please try again.');
     }
   };
+
   const handleWeightChange = event => {
     setWeight(event.target.value);
   };
+
   const handleActivityTimeChange = event => {
     setActivityTime(event.target.value);
   };
+
   const handleDrankWaterChange = event => {
     setDrankWaterAmount(event.target.value);
   };
+
   const calculateWaterAmount = useCallback(() => {
     const calculatedAmount = weight * 0.03 + activityTime * 0.4;
     setCalculatedWaterAmount(calculatedAmount.toFixed(2));
   }, [weight, activityTime]);
+
   useEffect(() => {
     calculateWaterAmount();
   }, [calculateWaterAmount]);
+
   useEffect(() => {
     setModalOpen(true);
   }, [setModalOpen]);
+
   return (
     <Box>
-      <Styled.ModalHeader>
-        {' '}
-        My daily norma
-        <Styled.CloseButton onClick={closeModal}>
-          <Icon
-            name="plus"
-            width={24}
-            height={24}
-            fill="#ffffff00"
-            stroke="#000000"
-            className="icon"
-          />
-        </Styled.CloseButton>
-      </Styled.ModalHeader>
-      <Styled.DailyNormas>
-        <>For girl:</>
-        <Styled.ColorTextNormal>V=(M*0.03) + (T*0.4)</Styled.ColorTextNormal>
-        <Styled.Forman>For man:</Styled.Forman>
-        <Styled.ColorTextNormal>V=(M*0.04) + (T*0.6)</Styled.ColorTextNormal>
-      </Styled.DailyNormas>
-      <Styled.VolumeNorm>
-        <Styled.TextNormal>
-          *V is the volume of the water norm in liters per day, M is your body
-          weight, T is the time of active sports, or another type of activity
-          commensurate in terms of loads (in the absence of these, you must set
-          0)
-        </Styled.TextNormal>
-      </Styled.VolumeNorm>
-      <Styled.CalculateYourRate>Calculate your rate:</Styled.CalculateYourRate>
-      <Styled.FrameParent>
-        <Styled.FrameItem type="radio" id="forGirl" />
-        <>For girl</>
-        <Styled.FrameItem type="radio" id="forMan" />
-        <>For man</>
-      </Styled.FrameParent>
-      <Styled.YourWeight>
-        <>Your weight in kilograms:</>
-        <FormInput
-          inputType="settings"
-          value={weight}
-          onChange={handleWeightChange}
-        />
-      </Styled.YourWeight>
-      <Styled.YourTime>
-        <p>
-          The time of active participation in sports or other activities with a
-          high physical. load:
-        </p>
-        <FormInput
-          inputType="settings"
-          value={activityTime}
-          onChange={handleActivityTimeChange}
-        />
-      </Styled.YourTime>
-      <Styled.Required>
-        <p>
-          The required amount of water in liters per day:
-          <Styled.L>{calculatedWaterAmount} L</Styled.L>
-        </p>
-      </Styled.Required>
-      <Styled.Write>
-        <p>Write down how much water you will drink:</p>
-      </Styled.Write>
-      <Styled.Frame>
-        <FormInput
-          inputType="dailyNorma"
-          value={drankWaterAmount}
-          onChange={handleDrankWaterChange}
-        />
-      </Styled.Frame>
-      <Styled.SaveWrapper>
-        <Button onClick={handleSave}>Save</Button>
-      </Styled.SaveWrapper>
+      {modalOpen && (
+        <Modal onClose={closeModal}>
+          <ModalHeader>
+            My daily norma
+            <CloseButton onClick={closeModal}>
+              <Icon
+                name="plus"
+                width={24}
+                height={24}
+                fill="#ffffff00"
+                stroke="#000000"
+                className="icon"
+              />
+            </CloseButton>
+          </ModalHeader>
+          <DailyNormas>
+            <>For girl:</>
+            <ColorTextNormal>V=(M*0.03) + (T*0.4)</ColorTextNormal>
+            <Forman>For man:</Forman>
+            <ColorTextNormal>V=(M*0.04) + (T*0.6)</ColorTextNormal>
+          </DailyNormas>
+          <VolumeNorm>
+            <TextNormal>
+              *V is the volume of the water norm in liters per day, M is your
+              body weight, T is the time of active sports, or another type of
+              activity commensurate in terms of loads (in the absence of these,
+              you must set 0)
+            </TextNormal>
+          </VolumeNorm>
+          <CalculateYourRate>Calculate your rate:</CalculateYourRate>
+          <FrameParent>
+            <FrameItem type="radio" id="forGirl" />
+            <>For girl</>
+            <FrameItem type="radio" id="forMan" />
+            <>For man</>
+          </FrameParent>
+          <YourWeight>
+            <>Your weight in kilograms:</>
+            <FormInput
+              inputType="settings"
+              value={weight}
+              onChange={handleWeightChange}
+            />
+          </YourWeight>
+          <YourTime>
+            <p>
+              The time of active participation in sports or other activities
+              with a high physical. load:
+            </p>
+            <FormInput
+              inputType="settings"
+              value={activityTime}
+              onChange={handleActivityTimeChange}
+            />
+          </YourTime>
+          <Required>
+            <p>
+              The required amount of water in liters per day:
+              <L>{calculatedWaterAmount} L</L>
+            </p>
+          </Required>
+          <Write>
+            <p>Write down how much water you will drink:</p>
+          </Write>
+          <Frame>
+            <FormInput
+              inputType="dailyNorma"
+              value={drankWaterAmount}
+              onChange={handleDrankWaterChange}
+            />
+          </Frame>
+          <SaveWrapper>
+            <Button onClick={handleSave}>Save</Button>
+          </SaveWrapper>
+        </Modal>
+      )}
     </Box>
   );
 };
