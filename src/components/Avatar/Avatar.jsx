0954082a96f27181f6476b55900afbe09e27df3b
Supreme-cake-being <XGon20987@gmail.com>
2023-11-16 16:24:34 +0200

    fixed error after refresh

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -5,14 +5,16 @@ import { color } from 'styles/colors.js';
 
 const Avatar = () => {
   const name = useSelector(state => state.auth.user.username);
-  // const firstLetter = name.slice(0, 1);
+  const firstLetter = name.slice(0, 1);
 
   if (name) {
     return (
       <AvatarBtn>
         <UserName>{name}</UserName>
         <>
-          <Circle>{/* <p>{firstLetter.toUpperCase()}</p> */}</Circle>
+          <Circle>
+            <p>{firstLetter.toUpperCase()}</p>
+          </Circle>
           <Icon
             name="arrow-down"
             fill={color.primary.blue}
