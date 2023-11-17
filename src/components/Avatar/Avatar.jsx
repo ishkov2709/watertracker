785781aa72aa8c1684c1cf58ff53b050889a5093
TamaraKhrustalyova <tamara.khrustalyova@gmail.com>
2023-11-17 16:15:24 +0200

    avatar

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -6,19 +6,18 @@ import { color } from 'styles/colors.js';
 
 const Avatar = () => {
   const name = useSelector(state => state.auth.user.username);
-  const email = useSelector(state => state.auth.user.email);
   const avatar = useSelector(state => state.auth.user.avatarURL);
   const firstLetter = name.slice(0, 1);
-  const index = email.indexOf('@');
-  const emailName = email.slice(0, index);
+  const index = name.indexOf('@');
+  const emailName = name.slice(0, index);
 
   return (
     <AvatarBtn>
       {(name.includes('@')) ? (<UserName>{emailName}</UserName>) : (<UserName>{name}</UserName>)}
       <>
         <Circle>
-          {name ? (<p>{firstLetter.toUpperCase()}</p>) : (<p>V</p>) }
-          {avatar && <img scr={avatar} alt="logo" />}
+          <p>{firstLetter.toUpperCase()}</p>
+          {avatar && <img src={avatar} alt="logo" />}
         </Circle>
         <Icon
           name="arrow-down"
