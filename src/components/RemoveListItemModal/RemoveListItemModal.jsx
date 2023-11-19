7b74a8b63f9c2bbd23e3579a20b7cfbb1ea113fe
Mozgarik <mozgarik1613@gmail.com>
2023-11-19 18:58:28 +0300

    update

--- src/components/RemoveListItemModal/RemoveListItemModal.jsx
+++ src/components/RemoveListItemModal/RemoveListItemModal.jsx
@@ -1,12 +1,14 @@
 import Modal from "components/common/Modal"
 import Button from "components/common/Button"
+import styled from "styled-components"
 import { useDispatch } from "react-redux"
 import { useContext } from "react"
 import { ModalContext } from "components/common/ModalProvider/ModalProvider"
 import { deleteWaterTodayById } from "store/waterData/thunk"
 import { useSelector } from "react-redux/es/hooks/useSelector"
 import { dataTodaySelector } from "store/waterData/selectors"
-import { DeleteModalBox, ButtonBox, DeleteModalTitle, DeleteModalDescription, TitleBox, ButtonPadding, ButtonMargin } from "./RemoveListItemModal.styled"
+import Icon from "components/common/Icon"
+import { DeleteModalBox, ButtonBox, DeleteModalTitle, DeleteModalDescription, TitleBox, ButtonPadding, ButtonMargin, CloseButton, BrSpan } from "./RemoveListItemModal.styled"
 
 
 
@@ -23,20 +25,22 @@ const RemoveListItemModal = (id) => {
         toggleModal()   
     }
 
-    const onClickCancelBtn = () => {
+    const onClickCloseBtn = () => {
         toggleModal()
     }
     
     return <Modal onClose={toggleModal}>
                     <DeleteModalBox>
-
+                           
                         <TitleBox>
+                            <CloseButton onClick={onClickCloseBtn}><Icon width='15px' height='15px' name='close'/></CloseButton>
                             <DeleteModalTitle>Delete entry</DeleteModalTitle>
-                            <DeleteModalDescription>Are you sure you want to delete the entry?</DeleteModalDescription>
+                            <DeleteModalDescription>Are you sure you want to <BrSpan/> delete the entry?</DeleteModalDescription>
+                             
                         </TitleBox>
-                    
+                        
                         <ButtonBox>
-                            <Button buttonType="cancel" onClick={onClickCancelBtn}><ButtonPadding>Cancel</ButtonPadding></Button> 
+                            <Button buttonType="cancel" style={{boxShadow: "none"}} onClick={onClickCloseBtn}><ButtonPadding>Cancel</ButtonPadding></Button> 
                             <ButtonMargin><Button buttonType="delete" onClick={onClickDeleteBtn}><ButtonPadding>Delete</ButtonPadding></Button></ButtonMargin>
                         </ButtonBox>
                     </DeleteModalBox>
--- src/components/RemoveListItemModal/RemoveListItemModal.styled.jsx
+++ src/components/RemoveListItemModal/RemoveListItemModal.styled.jsx
@@ -4,16 +4,30 @@ export const DeleteModalBox = styled.div`
 display: flex;
 flex-direction: column;
 justify-content: space-between;
-width: 592px;
-height: 180px;
+width: 560px;
+height: 156px;
+
+@media screen and (max-width: 768px) {
+    width: 260px;
+    height: 256px;
+    padding: 8px 12px;
+  }
 `
 
 export const ButtonBox = styled.div`
+@media screen and (max-width: 768px) {
+   display: block;
+   margin-top: 20px;
+  }
+
 display: flex;
 justify-content: flex-end;
-margin-bottom: 16px
+margin-bottom: 4px;
 `
 export const DeleteModalTitle = styled.h2`
+@media screen and (max-width: 768px) {
+    line-height: 32px
+   }
     margin-top: 5px;
     margin-bottom: 25px;
     font-family: Roboto;
@@ -22,18 +36,45 @@ export const DeleteModalTitle = styled.h2`
     color: #2F2F2F;
 `
 export const DeleteModalDescription = styled.span`
+@media screen and (max-width: 768px) {
+    line-height: 20px
+   }
+
 font-family: Roboto;
 font-size: 18px;
 font-weight: 500;
-color: #2F2F2F;
+color: #2F2F2F;    
 `
 export const TitleBox = styled.div`
 
 `
 
 export const ButtonPadding = styled.span`
+@media screen and (max-width: 768px) {
+    width: 232px;
+    font-size: 16px;
+    font-weight: 500;
+   }
 padding: 0 25px;
 `
 export const ButtonMargin = styled.span`
 margin-left: 40px
+`
+
+export const CloseButton = styled.button`
+position: absolute;
+margin-left: 538px;
+margin-top: 5px;
+background: none;
+
+@media screen and (max-width: 768px) {
+    margin-left: 210px;
+    margin-top: 9px;
+   }
+`
+
+export const BrSpan = styled.span`
+@media screen and (max-width: 768px) {
+    margin-left: 10px;
+   }
 `
