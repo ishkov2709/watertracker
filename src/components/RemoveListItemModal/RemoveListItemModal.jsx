50d4961d66008186f1535b4727f09584c5db8ed5
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-11-19 21:44:19 +0200

    fixed styles

--- src/components/RemoveListItemModal/RemoveListItemModal.jsx
+++ src/components/RemoveListItemModal/RemoveListItemModal.jsx
@@ -1,50 +1,70 @@
-import Modal from "components/common/Modal"
-import Button from "components/common/Button"
-import styled from "styled-components"
-import { useDispatch } from "react-redux"
-import { useContext } from "react"
-import { ModalContext } from "components/common/ModalProvider/ModalProvider"
-import { deleteWaterTodayById } from "store/waterData/thunk"
-import { useSelector } from "react-redux/es/hooks/useSelector"
-import { dataTodaySelector } from "store/waterData/selectors"
-import Icon from "components/common/Icon"
-import { DeleteModalBox, ButtonBox, DeleteModalTitle, DeleteModalDescription, TitleBox, ButtonPadding, ButtonMargin, CloseButton, BrSpan } from "./RemoveListItemModal.styled"
+import Modal from 'components/common/Modal';
+import Button from 'components/common/Button';
+import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+import { useContext } from 'react';
+import { ModalContext } from 'components/common/ModalProvider/ModalProvider';
+import { deleteWaterTodayById } from 'store/waterData/thunk';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { dataTodaySelector } from 'store/waterData/selectors';
+import Icon from 'components/common/Icon';
+import {
+  DeleteModalBox,
+  ButtonBox,
+  DeleteModalTitle,
+  DeleteModalDescription,
+  TitleBox,
+  ButtonPadding,
+  ButtonMargin,
+  CloseButton,
+  BrSpan,
+} from './RemoveListItemModal.styled';
 
+const RemoveListItemModal = id => {
+  const WaterItem = useSelector(dataTodaySelector);
+  const toggleModal = useContext(ModalContext);
 
+  const dispatch = useDispatch();
 
+  const onClickDeleteBtn = () => {
+    dispatch(deleteWaterTodayById(id.id));
+    toggleModal();
+  };
 
+  const onClickCloseBtn = () => {
+    toggleModal();
+  };
 
-const RemoveListItemModal = (id) => {
-    const WaterItem = useSelector(dataTodaySelector)
-    const toggleModal = useContext(ModalContext);
+  return (
+    <Modal onClose={toggleModal}>
+      <DeleteModalBox>
+        <TitleBox>
+          <CloseButton onClick={onClickCloseBtn}>
+            <Icon width="15px" height="15px" name="close" />
+          </CloseButton>
+          <DeleteModalTitle>Delete entry</DeleteModalTitle>
+          <DeleteModalDescription>
+            Are you sure you want to <BrSpan /> delete the entry?
+          </DeleteModalDescription>
+        </TitleBox>
 
-    const dispatch = useDispatch();
+        <ButtonBox>
+          <Button
+            buttonType="cancel"
+            style={{ boxShadow: 'none' }}
+            onClick={onClickCloseBtn}
+          >
+            <ButtonPadding>Cancel</ButtonPadding>
+          </Button>
+          <ButtonMargin>
+            <Button buttonType="delete" onClick={onClickDeleteBtn}>
+              <ButtonPadding>Delete</ButtonPadding>
+            </Button>
+          </ButtonMargin>
+        </ButtonBox>
+      </DeleteModalBox>
+    </Modal>
+  );
+};
 
-    const onClickDeleteBtn = () => {
-        dispatch(deleteWaterTodayById(id.id))
-        toggleModal()   
-    }
-
-    const onClickCloseBtn = () => {
-        toggleModal()
-    }
-    
-    return <Modal onClose={toggleModal}>
-                    <DeleteModalBox>
-                           
-                        <TitleBox>
-                            <CloseButton onClick={onClickCloseBtn}><Icon width='15px' height='15px' name='close'/></CloseButton>
-                            <DeleteModalTitle>Delete entry</DeleteModalTitle>
-                            <DeleteModalDescription>Are you sure you want to <BrSpan/> delete the entry?</DeleteModalDescription>
-                             
-                        </TitleBox>
-                        
-                        <ButtonBox>
-                            <Button buttonType="cancel" style={{boxShadow: "none"}} onClick={onClickCloseBtn}><ButtonPadding>Cancel</ButtonPadding></Button> 
-                            <ButtonMargin><Button buttonType="delete" onClick={onClickDeleteBtn}><ButtonPadding>Delete</ButtonPadding></Button></ButtonMargin>
-                        </ButtonBox>
-                    </DeleteModalBox>
-            </Modal>
-}
-
-export default RemoveListItemModal
+export default RemoveListItemModal;
--- src/components/RemoveListItemModal/RemoveListItemModal.styled.jsx
+++ src/components/RemoveListItemModal/RemoveListItemModal.styled.jsx
@@ -1,80 +1,83 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
 export const DeleteModalBox = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-width: 560px;
-height: 156px;
+  width: 256px;
+  height: 232px;
+  padding: 8px 12px;
 
-@media screen and (max-width: 768px) {
-    width: 260px;
-    height: 256px;
-    padding: 8px 12px;
+  @media screen and (min-width: 767px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 544px;
+    height: 144px;
   }
-`
+`;
 
 export const ButtonBox = styled.div`
-@media screen and (max-width: 768px) {
-   display: block;
-   margin-top: 20px;
+  display: block;
+  margin-top: 20px;
+
+  @media screen and (min-width: 767px) {
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 4px;
   }
-
-display: flex;
-justify-content: flex-end;
-margin-bottom: 4px;
-`
+`;
 export const DeleteModalTitle = styled.h2`
-@media screen and (max-width: 768px) {
-    line-height: 32px
-   }
-    margin-top: 5px;
-    margin-bottom: 25px;
-    font-family: Roboto;
-    font-size: 26px;
-    font-weight: 500;
-    color: #2F2F2F;
-`
+  margin-top: 5px;
+  margin-bottom: 25px;
+  font-family: Roboto;
+  font-size: 26px;
+  font-weight: 500;
+  color: #2f2f2f;
+
+  @media screen and (max-width: 767px) {
+    line-height: 32px;
+  }
+`;
 export const DeleteModalDescription = styled.span`
-@media screen and (max-width: 768px) {
-    line-height: 20px
-   }
+  font-family: Roboto;
+  font-size: 18px;
+  font-weight: 500;
+  color: #2f2f2f;
 
-font-family: Roboto;
-font-size: 18px;
-font-weight: 500;
-color: #2F2F2F;    
-`
-export const TitleBox = styled.div`
-
-`
+  @media screen and (max-width: 767px) {
+    line-height: 20px;
+  }
+`;
+export const TitleBox = styled.div``;
 
 export const ButtonPadding = styled.span`
-@media screen and (max-width: 768px) {
+  padding: 0 25px;
+
+  @media screen and (max-width: 767px) {
     width: 232px;
     font-size: 16px;
     font-weight: 500;
-   }
-padding: 0 25px;
-`
+  }
+`;
 export const ButtonMargin = styled.span`
-margin-left: 40px
-`
+  margin-left: 40px;
+`;
 
 export const CloseButton = styled.button`
-position: absolute;
-margin-left: 538px;
-margin-top: 5px;
-background: none;
+  position: absolute;
+  background: none;
 
-@media screen and (max-width: 768px) {
+  @media screen and (max-width: 768px) {
     margin-left: 210px;
     margin-top: 9px;
-   }
-`
+  }
+
+  @media screen and (min-width: 767px) {
+    margin-left: 508px;
+    margin-top: 5px;
+  }
+`;
 
 export const BrSpan = styled.span`
-@media screen and (max-width: 768px) {
+  @media screen and (max-width: 767px) {
     margin-left: 10px;
-   }
-`
+  }
+`;
